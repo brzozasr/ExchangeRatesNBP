2f24181e2e72b6a69dc7bd3f43d17ddada3f85a4
brzozasr <brzozasr@interia.eu>
2019-11-06 22:26:45 +0100

    Exchange Rates NBP 02

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,18 +1,16 @@
 package main;
 
-import utils.IncorrectXmlException;
-
 import java.io.IOException;
 
-import static utils.DataFromURL.getXml;
+import static utils.httpData.readXMLToString;
 
 public class Main {
 
     static public void main(String[] args) {
         try {
-            String xml = getXml("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-01/2013-12-31/");
+            String xml = readXMLToString("http://api.nbp.pl/api/exchangerates/rates/a/gbp/2012-01-01/2013-01-02/?format=xml");
             System.out.println(xml);
-        } catch (IncorrectXmlException | IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
--- src/main/java/utils/IncorrectXmlException.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class IncorrectXmlException extends Exception {
-    public IncorrectXmlException(String errorMessage) {
-        super(errorMessage);
-    }
-}
