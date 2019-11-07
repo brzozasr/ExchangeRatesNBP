e075d3c74bb570ece7d11da8f5da69d14177dbc9
brzozasr <brzozasr@interia.eu>
2019-11-07 10:57:01 +0100

    Exchange Rates NBP 05

--- src/main/java/http/LinksTableA.java
+++ src/main/java/http/LinksTableA.java
@@ -1,5 +1,8 @@
 package http;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Linki do kursów średnich walut obcych w złotych określonych w § 2 pkt 1 i 2 uchwały Nr 51/2002
  * Zarządu Narodowego Banku Polskiego z dnia 23 września 2002 r. w sprawie sposobu wyliczania
@@ -12,7 +15,7 @@ public class LinksTableA {
      *
      * @return String - link do xml
      */
-    public String current() {
+    public String currentTable() {
         return "http://api.nbp.pl/api/exchangerates/tables/a/?format=xml";
     }
 
@@ -23,7 +26,43 @@ public class LinksTableA {
      *                 danych (limit wyników 67)
      * @return String - link do xml
      */
-    public String lastTopCount(int topCount) {
+    public String lastTopCountTables(int topCount) {
         return "http://api.nbp.pl/api/exchangerates/tables/a/last/" + topCount + "/?format=xml";
+    }
+
+    /**
+     * Tabela kursów typu A opublikowana w dniu dzisiejszym (albo brak danych)
+     *
+     * @return String - link do xml
+     */
+    public String tablePublishedToday() {
+        return "http://api.nbp.pl/api/exchangerates/tables/a/today/?format=xml";
+    }
+
+    /**
+     * Tabela kursów typu A opublikowana w dniu {date} (albo brak danych)
+     *
+     * @param date data w formacie rrrr-MM-dd (standard ISO 8601)
+     * @return link do xml
+     */
+    public String tablePublishedOnDate(LocalDate date) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formatDate = dateTimeFormatter.format(date);
+        return "http://api.nbp.pl/api/exchangerates/tables/a/" + formatDate + "/?format=xml";
+    }
+
+    /**
+     * Seria tabel kursów typu A opublikowanych w zakresie dat od {startDate} do {endDate} (albo brak danych),
+     * (limit dni 93)
+     *
+     * @param startDate data w formacie rrrr-MM-dd (standard ISO 8601)
+     * @param endDate   data w formacie rrrr-MM-dd (standard ISO 8601)
+     * @return link do xml
+     */
+    public String tablesPublishedOnDateRange(LocalDate startDate, LocalDate endDate) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String startFormatDate = dateTimeFormatter.format(startDate);
+        String endFormatDate = dateTimeFormatter.format(endDate);
+        return "http://api.nbp.pl/api/exchangerates/tables/a/" + startFormatDate + "/" + endFormatDate + "/?format=xml";
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,6 +3,7 @@ package main;
 import http.LinksTableA;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 import static http.ReadHttpData.readXMLToString;
 
@@ -10,7 +11,7 @@ public class Main {
 
     static public void main(String[] args) {
         try {
-            String xml = readXMLToString(new LinksTableA().lastTopCount(67));
+            String xml = readXMLToString(new LinksTableA().lastTopCountTables(68));
             System.out.println(xml);
         } catch (IOException e) {
             e.printStackTrace();
